36d1e02dd88633665c4e6e34836fa44a317f5250
Haltimo Samuli <samuli.haltimo@metropolia.fi>
2023-02-05 10:28:41 +0200

    Working checkpoint animation

--- Assets/Scripts/Checkpoiint.cs
+++ Assets/Scripts/Checkpoiint.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class Checkpoiint : MonoBehaviour
 {
-    Animator animator;
+    public Animator animator;
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player"))
         {
             animator.SetBool("PlayAnimation", true);
         }
